52ccd945fa725a3c80eed0a3a250d1f4e56d9ce4
slno1011 <slno1011@hs-karlsruhe.de>
2023-01-03 12:26:43 +0100

    Kunde-Klasse deklariert

--- src/main/java/de/hska/iwii/db1/jpa/Kunde.java
+++ src/main/java/de/hska/iwii/db1/jpa/Kunde.java
@@ -1,6 +1,45 @@
 package de.hska.iwii.db1.jpa;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "kunde")
 public class Kunde {
 	@Id
 	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	@Column(name = "vorname")
+	private String vorname;
+
+	@Column(name = "nachname")
+	private String nachname;
+
+	@Column(name = "email")
+	private String email;
+
+// Setter und Getter
+	public Kunde(String vorname, String nachname, String email) {
+		this.vorname = vorname;
+		this.nachname = nachname;
+		this.email = email;
+	}
+
+	public void setID(int id) { this.id = id; }
+	public int getID() { return this.id; }
+
+	public void setVorname(String vorname) { this.vorname = vorname; }
+	public String getVorname() { return this.vorname; }
+
+	public void setNachname(String nachname) { this.nachname = nachname; }
+	public String getNachname() { return this.nachname; }
+
+	public void setEmail(String email) { this.email = email; }
+	public String getEmail() { return this.email; }
 }
